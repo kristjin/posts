ce4f5f7113e65d3de3dfb30cde716e2a62bc5e42
Matthew Conquergood <matthew.conquergood@gmail.com>
2015-03-10 01:24:32 +0000

    Working DB retrieval.

--- posts/api.py
+++ posts/api.py
@@ -8,3 +8,12 @@ import decorators
 from posts import app
 from database import session
 
+@app.route("/api/posts", methods=["GET"])
+def posts_get():
+    """ Get a list of posts """
+    # Obtain the posts from the database
+    posts = session.query(models.Post).all()
+    # Convert the data to JSON format
+    data = json.dumps([post.as_dictionary() for post in posts])
+    # Return a 200 status and the data
+    return Response(data, 200, mimetype="application/json")
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -20,6 +20,45 @@ class TestAPI(unittest.TestCase):
         # Set up the tables in the database
         Base.metadata.create_all(engine)
 
+    def testGetPosts(self):
+        """ Getting posts from a populated database """
+        # Create a couple of sample posts
+        postA = models.Post(title="Example Post A", body="Just a test")
+        postB = models.Post(title="Example Post B", body="Still a test")
+        # Add them to the session and commit
+        session.add_all([postA, postB])
+        session.commit()
+        # Go to the page and get the response from the server, store it here
+        response = self.client.get("/api/posts")
+        # Was the request to the endpoint successful?
+        self.assertEqual(response.status_code, 200)
+        # Did the request return a JSON object?
+        self.assertEqual(response.mimetype, "application/json")
+        # Decode the data using json.loads
+        data = json.loads(response.data)
+        # Verify that two posts have been returned
+        self.assertEqual(len(data), 2)
+        # Verify the contents of both posts as correct
+        postA = data[0]
+        self.assertEqual(postA["title"], "Example Post A")
+        self.assertEqual(postA["body"], "Just a test")
+        postB = data[1]
+        self.assertEqual(postB["title"], "Example Post B")
+        self.assertEqual(postB["body"], "Still a test")
+
+    def testGetEmptyPosts(self):
+        """ Getting posts from an empty database """
+        # Go to the page and get the response from the server, store it here
+        response = self.client.get("/api/posts")
+        # Was the request to the endpoint successful?
+        self.assertEqual(response.status_code, 200)
+        # Did the request return a JSON object?
+        self.assertEqual(response.mimetype, "application/json")
+        # Decode the data using json.loads
+        data = json.loads(response.data)
+        # Is the JSON empty (because this tests an empty db)
+        self.assertEqual(data, [])
+
     def tearDown(self):
         """ Test teardown """
         session.close()
