57a92c301d2b2998a237931a6d0f1225b10a007c
Matthew Conquergood <matthew.conquergood@gmail.com>
2015-03-10 15:52:56 +0000

    Adding a Post functional

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -3,12 +3,13 @@ import os
 import json
 from urlparse import urlparse
 
-# Configure our app to use the testing databse
+# Configure our app to use the testing database
 os.environ["CONFIG_PATH"] = "posts.config.TestingConfig"
 
 from posts import app
 from posts import models
 from posts.database import Base, engine, session
+
 
 class TestAPI(unittest.TestCase):
     """ Tests for the posts API """
@@ -16,23 +17,70 @@ class TestAPI(unittest.TestCase):
     def setUp(self):
         """ Test setup """
         self.client = app.test_client()
-
         # Set up the tables in the database
         Base.metadata.create_all(engine)
 
+    def testPostPost(self):
+        """ Posting a new post """
+        # Compile post data into a dictionary for easy conversion to JSON
+        data = {
+            "title": "Example Post",
+            "body": "Just a test"
+        }
+
+        # Collect the response from the endpoint
+        # use json.dumps to convert dict > JSON
+        # use content_type to indicate the type of content in data
+        response = self.client.post("/api/posts",
+                                    data=json.dumps(data),
+                                    content_type="application/json",
+                                    headers=[("Accept", "application/json")],
+                                    )
+
+        # Verify request to endpoint was successful using 201 created
+        self.assertEqual(response.status_code, 201)
+        # Verify that the response is JSON type
+        self.assertEqual(response.mimetype, "application/json")
+        # Verify the endpoint is setting the correct Location header
+        # This should be the link to the new post
+        self.assertEqual(urlparse(response.headers.get("Location")).path,
+                         "/api/posts/1")
+        # Decode the response with json.loads
+        data = json.loads(response.data)
+        # Validate the id, title and body
+        self.assertEqual(data["id"], 1)
+        self.assertEqual(data["title"], "Example Post")
+        self.assertEqual(data["body"], "Just a test")
+        # Query DB to validate status
+        posts = session.query(models.Post).all()
+        # Verify only one item in DB
+        self.assertEqual(len(posts), 1)
+        # Isolate the first post in the list received
+        post = posts[0]
+        # Validate the content of the item retrieved from the DB
+        self.assertEqual(post.title, "Example Post")
+        self.assertEqual(post.body, "Just a test")
+
+
+
     def testGetPostsWithTitle(self):
         """ Filtering posts by title """
-        postA = models.Post(title="Post with bells", body="Just a test")
-        postB = models.Post(title="Post with whistles", body="Still a test")
+        postA = models.Post(title="Post with bells",
+                            body="Just a test",
+                            )
+        postB = models.Post(title="Post with whistles",
+                            body="Still a test",
+                            )
         postC = models.Post(title="Post with bells and whistles",
-                            body="Another test")
+                            body="Another test",
+                            )
 
         session.add_all([postA, postB, postC])
         session.commit()
 
         response = self.client.get("/api/posts?title_like=whistles",
-            headers=[("Accept", "application/json")]
-        )
+                                   headers=[("Accept", "application/json")],
+                                   )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, "application/json")
@@ -50,17 +98,22 @@ class TestAPI(unittest.TestCase):
 
     def testGetPostsWithTitleandBody(self):
         """ Filtering posts by title """
-        postA = models.Post(title="Post with bells", body="No whistles")
-        postB = models.Post(title="Post with whistles", body="No bells")
+        postA = models.Post(title="Post with bells",
+                            body="No whistles",
+                            )
+        postB = models.Post(title="Post with whistles",
+                            body="No bells",
+                            )
         postC = models.Post(title="Post with bells and whistles",
-                            body="Both bells and whistles")
+                            body="Both bells and whistles",
+                            )
 
         session.add_all([postA, postB, postC])
         session.commit()
 
         response = self.client.get("/api/posts?title_like=whistles&body_like=bells",
-            headers=[("Accept", "application/json")]
-        )
+                                   headers=[("Accept", "application/json")],
+                                   )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, "application/json")
@@ -86,8 +139,8 @@ class TestAPI(unittest.TestCase):
         session.commit()
         # Go to the page and get the response from the server, store it here
         response = self.client.get("/api/posts",
-                                   headers=[("Accept", "application/json")]
-        )
+                                   headers=[("Accept", "application/json")],
+                                   )
         # Was the request to the endpoint successful?
         self.assertEqual(response.status_code, 200)
         # Did the request return a JSON object?
@@ -108,8 +161,8 @@ class TestAPI(unittest.TestCase):
         """ Getting posts from an empty database """
         # Go to the page and get the response from the server, store it here
         response = self.client.get("/api/posts",
-                                   headers=[("Accept", "application/json")]
-        )
+                                   headers=[("Accept", "application/json")],
+                                   )
         # Was the request to the endpoint successful?
         self.assertEqual(response.status_code, 200)
         # Did the request return a JSON object?
@@ -128,8 +181,8 @@ class TestAPI(unittest.TestCase):
         session.commit()
 
         response = self.client.get("/api/posts/{}".format(postB.id),
-                                   headers=[("Accept", "application/json")]
-        )
+                                   headers=[("Accept", "application/json")],
+                                   )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, "application/json")
@@ -147,8 +200,8 @@ class TestAPI(unittest.TestCase):
         session.commit()
 
         response = self.client.delete("/api/posts/{}".format(postB.id),
-                                      headers=[("Accept", "application/json")]
-        )
+                                      headers=[("Accept", "application/json")],
+                                      )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, "application/json")
@@ -159,8 +212,8 @@ class TestAPI(unittest.TestCase):
     def testDeleteNonExistentPost(self):
         """ Trying to Delete a non-existent Post """
         response = self.client.delete("/api/posts/1",
-                                      headers=[("Accept", "application/json")]
-        )
+                                      headers=[("Accept", "application/json")],
+                                      )
 
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.mimetype, "application/json")
@@ -171,8 +224,8 @@ class TestAPI(unittest.TestCase):
     def testGetNonExistentPost(self):
         """ Getting a single post which doesn't exist """
         response = self.client.get("/api/posts/1",
-                                   headers=[("Accept", "application/json")]
-        )
+                                   headers=[("Accept", "application/json")],
+                                   )
 
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.mimetype, "application/json")
@@ -182,8 +235,8 @@ class TestAPI(unittest.TestCase):
 
     def testUnsupportedAcceptHeader(self):
         response = self.client.get("/api/posts",
-            headers=[("Accept", "application/xml")]
-        )
+                                   headers=[("Accept", "application/xml")],
+                                   )
 
         self.assertEqual(response.status_code, 406)
         self.assertEqual(response.mimetype, "application/json")
